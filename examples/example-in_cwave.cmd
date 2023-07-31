6b072a2e911966fba3aa7f782c3749e7c0cb4e8d
rat-and-catcher <67433995+rat-and-catcher@users.noreply.github.com>
2023-07-31 21:49:32 +0300

    small example scripts
    scripts to gen filters for our in_cwave project Hilbert transformer

---
+++ examples/example-in_cwave.cmd
@@ -0,0 +1,12 @@
+@echo off
+REM build Hilbert transformer filters for in_cwave.dll plugin
+
+gen-iir -j -o:chexui
+
+gen-iir -w -o:chexui -t:lpf -a:ellip -p:1.5 -s:100 -n:15 -l:0.495
+gen-iir -w -o:chexui -t:lpf -a:ellip -p:1.8 -s:100 -n:19 -l:0.499
+gen-iir -w -o:chexui -t:lpf -a:ellip -p:2.0 -s:90  -n:18 -l:0.499
+
+gen-iir -w -o:chexui -t:lpf -a:ellip -p:2.0 -s:96  -n:19 -l:0.4982
+gen-iir -w -o:chexui -t:lpf -a:ellip -p:2.0 -s:100 -n:20 -l:0.4982
+gen-iir -w -o:chexui -t:lpf -a:ellip -p:2.0 -s:100 -n:20 -l:0.4985
